eb0bedf48f52d5cabca4073a5fa814bdbf1eae0d
Luis Felipe Bertel Mercado <ellipe@gmail.com>
2022-08-10 16:59:13 -0500

    auth guard at route level

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Query,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -19,6 +20,7 @@ import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -29,6 +31,7 @@ export class UsersController {
   ) {}
 
   @Get('/whoami')
+  @UseGuards(AuthGuard)
   whoami(@CurrentUser() user: User) {
     return user;
   }
@@ -63,8 +66,6 @@ export class UsersController {
 
   @Get()
   findAllUsers(@Query('email') email: string) {
-    console.log('handler is running...');
-
     return this.userService.find(email);
   }
 
